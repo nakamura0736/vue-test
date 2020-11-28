1c67c1f9adbf4bcdb6caba219976850c0c08d6dc
nakamura0736 <tel07360102@gmail.com>
2020-11-28 12:33:18 +0900

    delete helloworld

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 import Top from '@/components/top'
 import Contact from '@/components/contact'
 import About from '@/components/about'
